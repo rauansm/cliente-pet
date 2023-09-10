02b48a258f6d9c359acad648cc1eda7051953fde
Rauan Santos <rauanmor@gmail.com>
2023-09-10 20:28:05 -0300

    Implementa contrato da API

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -1,12 +1,9 @@
 package br.com.petz.clientepet;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/")
