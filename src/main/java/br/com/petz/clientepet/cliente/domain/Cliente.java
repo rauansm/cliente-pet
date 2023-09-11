7e201c54649a30200f698cf5cde077799eacb755
Rauan Santos <rauanmor@gmail.com>
2023-09-10 21:44:16 -0300

    implementa controler

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -1,6 +1,6 @@
 package br.com.petz.clientepet.cliente.domain;
 
-import jakarta.persistence.*;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,13 +9,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.UUID;
 import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.annotation.Id;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Entity
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -40,7 +46,6 @@ public class Cliente {
     private LocalDateTime dataHoraDaUltimaAlteracao;
 
 
-
     public Cliente(String nomeCompleto, String email, String celular, String telefone, Sexo sexo, LocalDate dataNascimento, String cpf, Boolean aceitaTermos) {
         this.nomeCompleto = nomeCompleto;
         this.email = email;
@@ -53,3 +58,4 @@ public class Cliente {
         this.dataHoraDoCadastro = LocalDateTime.now();
     }
 }
+
