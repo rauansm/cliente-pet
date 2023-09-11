40983a164567ae678d5409939b45e28ff39b7c9b
Rauan Santos <rauanmor@gmail.com>
2023-09-10 22:06:12 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
@@ -1,4 +1,8 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import lombok.Value;
+import org.hibernate.validator.constraints.UUID;
+@Value
 public class ClienteResponse {
+    private UUID idCliente;
 }
