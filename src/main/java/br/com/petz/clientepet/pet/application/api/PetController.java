9f7542548df450a68e9462f37a66695d78a5b7a0
Rauan Santos <rauanmor@gmail.com>
2023-09-15 15:42:40 -0300

    implementa fluxo controller lista todos pet

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 @RestController
 @Log4j2
@@ -18,5 +19,13 @@ public class PetController implements PetAPI {
         PetResponse petCriado = petService.criaPet(idCliente, petRequest);
         log.info("[finaliza] PetController - postPet");
         return petCriado;
+    }
+
+    @Override
+    public List<PetListResponse> getPetsDoClientePorId(UUID idCliente) {
+        log.info("[inicia] PetController - getPetsDoClientePorId");
+        log.info("[idCliente] {}", idCliente);
+        log.info("[finaliza] PetController - getPetsDoClientePorId");
+        return null;
     }
 }
