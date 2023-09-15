a4ae330b347d8bed0e1cefe509f1a1d2eeafa031
Rauan Santos <rauanmor@gmail.com>
2023-09-15 16:41:41 -0300

    implementa busca pets pelo idCliente

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetListResponse.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetListResponse.java
@@ -1,11 +1,35 @@
 package br.com.petz.clientepet.pet.application.api;
 
 import br.com.petz.clientepet.pet.domain.Pet;
+import br.com.petz.clientepet.pet.domain.TipoPet;
+import lombok.Value;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class PetListResponse {
+    private UUID idPet;
+    private String nomePet;
+    private TipoPet tipo;
+    private String raca;
+    private String pelagemCor;
+    private LocalDate dataNascimento;
+
     public static List<PetListResponse> converte(List<Pet> listaPet) {
-        return null;
+        return listaPet.stream()
+                .map(PetListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public PetListResponse(Pet pet) {
+        this.idPet = pet.getIdPet();
+        this.nomePet = pet.getNomePet();
+        this.tipo = pet.getTipo();
+        this.raca = pet.getRaca();
+        this.pelagemCor = pet.getPelagemCor();
+        this.dataNascimento = pet.getDataNascimento();
     }
 }
